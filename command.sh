044c4ee9f5a06437fcc6654e3c9650cbe3608a93
Yassine H.M <68260654+yassine-hamadou@users.noreply.github.com>
2023-08-31 00:41:27 +0000

    Update

--- command.sh
+++ command.sh
@@ -4,4 +4,8 @@ npm start
 git commit -m "First commit"
 gh repo create forage_project --public
 git checkout -b update_logo
+git add .
 git commit -m "Update logo"
+git remote add origin git@github.com:yassine-hamadou/forage_project.git
+git push -u origin update_logo
+
